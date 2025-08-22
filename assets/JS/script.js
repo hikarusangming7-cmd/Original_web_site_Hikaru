8bb4fdde3865ac390d8efa03dbc74ead85954123
hikarusangming7-cmd <hikarusangming7@gmail.com>
2025-08-22 14:26:09 +0800

    save

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -35,3 +35,42 @@
   $(document).on("click", function () {
     $(".dropdown-content").hide();
   });
+
+
+
+
+    $("#menu_Btn2").on("click", function (e) {
+    e.stopPropagation(); // 外側クリックの閉じる処理が先に走らないように
+    const $menu = $(this).next(".dropdown-content2");
+    $(".dropdown-content2").not($menu).hide(); // 他を閉じる
+    $menu.toggle();
+  });
+
+  // 項目クリック時の処理
+  $(".dropdown-content2").on("click", "button", function (e) {
+    e.stopPropagation();
+    const action = $(this).data("action");
+
+    if (action === "story") {
+      // ストーリーを選択した時の処理をここに
+      // 例: 別ページへ移動 or 画面内の表示切り替え
+      // location.href = "./story.html";
+      $("#story").trigger("click"); // 既存の #story ボタンの処理を使いたい場合
+    } else if (action === "ningyo") {
+      // 人形編の処理
+      // location.href = "./ningyo.html";
+      console.log("人形編を選択");
+    } else if (action === "okada") {
+      // 岡田さん編の処理
+      // location.href = "./okada.html";
+      console.log("岡田さん編を選択");
+    }
+
+    // 選択後は閉じる
+    $(this).closest(".dropdown-content2").hide();
+  });
+
+  // 画面のどこかをクリックしたら閉じる
+  $(document).on("click", function () {
+    $(".dropdown-content2").hide();
+  });
